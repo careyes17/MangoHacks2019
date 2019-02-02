9f292c82db9e0774583e666fbbe8c13b554477d5
tgayle <travisgayledev@gmail.com>
2019-02-02 11:40:23 -0500

    Hello basic toolbar!

---
+++ vueapp/src/components/AppToolbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-toolbar app>
+    <v-toolbar-title class="headline text-uppercase">
+      <span>PatientSafe</span>
+      <!-- <span class="font-weight-light">MATERIAL DESIGN</span> -->
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <!-- <v-btn
+      flat
+      href="https://github.com/vuetifyjs/vuetify/releases/latest"
+      target="_blank"
+    >
+      <span class="mr-2">Recent User Update</span>
+      <v-icon>account_circle</v-icon>
+    </v-btn> -->
+
+    <span class="mr-2">Recent User Update</span>
+    <v-btn flat>
+      <v-icon>account_circle</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
