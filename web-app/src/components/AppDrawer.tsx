795bdf3fc3efd3012f342b3340acfef4d548dc87
tgayle <travisgayledev@gmail.com>
2019-02-02 03:31:47 -0500

    Create AtAGlance file

--- web-app/src/components/AppDrawer.tsx
+++ web-app/src/components/AppDrawer.tsx
@@ -3,16 +3,18 @@ import PropTypes from 'prop-types';
 import {ListGroup} from 'react-bootstrap';
 
 class AppDrawer extends Component {
-  items = [1, 2, 3, 4, 5, 6];
+  items = [
+    "Prescriptions",
+    "Messages",
+    "Event Log",
+    "Google Home",
+    "Music",
+    "Contact Information"
+  ]
   render() {
     return (
       <ListGroup variant="flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-        {this.items.map(n => <ListGroup.Item>{n}</ListGroup.Item>)}
+        {this.items.map(option => <ListGroup.Item>{option}</ListGroup.Item>)}
       </ListGroup>
      );
   }
---
+++ web-app/src/components/AtAGlanceHome.tsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class AtAGlance extends Component {
+  render() {
+    return <div></div>
+  }
+}
+
+export default AtAGlance;
