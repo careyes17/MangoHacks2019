f732e2f2f5466c72c4a1dd35eeab2e9c5da2d28c
tgayle <travisgayledev@gmail.com>
2019-02-02 12:18:54 -0500

    Added components for each screen and added to router

--- vueapp/src/router.ts
+++ vueapp/src/router.ts
@@ -2,6 +2,13 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 
+import PrescriptionsVue from './screens/PrescriptionsVue.vue';
+import MessagesVue from './screens/MessagesVue.vue';
+import EventLogVue from './screens/EventLogVue.vue';
+import IntegrationsVue from './screens/IntegrationsVue.vue';
+import MusicVue from './screens/MusicVue.vue';
+import ContactInfoVue from './screens/ContactInfo.vue';
+
 Vue.use(Router);
 
 export default new Router({
@@ -18,6 +25,36 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    },
+    {
+      path: '/prescriptions',
+      name: 'Prescriptions',
+      component: PrescriptionsVue
+    },
+    {
+      path: '/messages',
+      name: 'Messages',
+      component: MessagesVue
+    },
+    {
+      path: '/events',
+      name: 'Event Log',
+      component: EventLogVue
+    },
+    {
+      path: '/integration',
+      name: 'Integrations',
+      component: IntegrationsVue
+    },
+    {
+      path: '/music',
+      name: 'Music',
+      component: MusicVue
+    },
+    {
+      path: '/contact',
+      name: 'Contact Information',
+      component: ContactInfoVue
     },
   ],
 });
